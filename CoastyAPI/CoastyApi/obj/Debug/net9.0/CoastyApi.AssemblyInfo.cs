11ba4081a60babf2fba46d929da7361269f8099b
aidanMCNAM <amcnam4@lsu.edu>
2025-11-25 16:03:06 -0600

    changes

--- CoastyAPI/CoastyApi/obj/Debug/net9.0/CoastyApi.AssemblyInfo.cs
+++ CoastyAPI/CoastyApi/obj/Debug/net9.0/CoastyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoastyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59e9985c0b5aef73a31691cff9b8535b547c8dfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26195e6f2545b7ea6adfe9435a0b8c4d9d37c832")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoastyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoastyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
